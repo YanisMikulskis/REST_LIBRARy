bef924586de5135dfc3e2830a904d570865410f4
Yanis Mikulskis <www.titan.ru95@mail.ru>
2025-01-13 00:05:31 +0300

    Начальный фронт настроен

--- library_pr/front-end/src/App.js
+++ library_pr/front-end/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import AuthorList from "./components/Author";
-
+import axios from 'axios';
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -12,40 +12,27 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    const authors = [
-      {
-        'first_name': 'Иван',
-        'last_name': 'Иванов',
-        'birthday_year': 1995
-      },
-      {
-        'first_name': 'Пётр',
-        'last_name': 'Сергеев',
-        'birthday_year': 2000
-      },
-      {
-        'first_name': 'Виктор',
-        'last_name': 'Иванов',
-        'birthday_year': 1950
-      }
-    ]
-    this.setState(
-        {
-          'authors': authors
-        }
-    )
+    axios.get('http://127.0.0.1:8000/api/author')
+        .then(response => {
+          const authors = response.data
+          this.setState(
+              {
+                'authors': authors
+              }
+          )
+        }).catch(error => console.log(error))
+
   }
 
-  render () {
+  render() {
     return (
         <div>
-          <AuthorList authors={this.state.authors} />
+          <AuthorList authors={this.state.authors}/>
         </div>
     )
   }
-
-
 }
+
 
 // function App() {
 //   return (
