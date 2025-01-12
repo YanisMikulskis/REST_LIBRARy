4c297bca87a3bd8d54cef689cedfc2f1f5c51e65
Yanis Mikulskis <www.titan.ru95@mail.ru>
2025-01-12 22:37:36 +0300

    Прописал компоненты

--- library_pr/front-end/src/App.js
+++ library_pr/front-end/src/App.js
@@ -1,25 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import AuthorList from "./components/Author";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      'authors': []
+    }
+  }
+
+  componentDidMount() {
+    const authors = [
+      {
+        'first_name': 'Иван',
+        'last_name': 'Иванов',
+        'birthday_year': 1995
+      },
+      {
+        'first_name': 'Пётр',
+        'last_name': 'Сергеев',
+        'birthday_year': 2000
+      },
+      {
+        'first_name': 'Виктор',
+        'last_name': 'Иванов',
+        'birthday_year': 1950
+      }
+    ]
+    this.setState(
+        {
+          'authors': authors
+        }
+    )
+  }
+
+  render () {
+    return (
+        <div>
+          <AuthorList authors={this.state.authors} />
+        </div>
+    )
+  }
+
+
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+
 
 export default App;
