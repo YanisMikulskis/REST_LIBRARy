e47f0bf3ec17f6a8d2cefd1aaf584635ba25db96
Yanis Mikulskis <www.titan.ru95@mail.ru>
2025-01-28 23:04:35 +0300

    Чек коммит

--- library_pr/author/tests.py
+++ library_pr/author/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+# from django.test import TestCase
 
 # Create your tests here.
+from rest_framework import serializers
+from models import AuthorModel
+class AuthorSerializers(serializers.Serializer):
+    name = serializers.CharField(max_length=256)
+    birthday = serializers.IntegerField()
+
+
+author = AuthorModel('Вася', 'Пупкин', 1995)
+print(author)
